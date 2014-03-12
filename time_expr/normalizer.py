c8822d8f57aef90d014afec0ad46338931c3e9ab
leon-huang <leon@njuopen.com>
2014-03-12 21:08:59 +0800

    normalizer nearly completed - TODO: test

--- time_expr/normalizer.py
+++ time_expr/normalizer.py
@@ -4,6 +4,7 @@
 import datetime
 
 import prehandler
+from unit import TimeUnit
 
 class ReadModelError(Exception):
     def __init__(self, path):
@@ -19,7 +20,7 @@ class TimeNormalizer:
             except Exception:
                 raise ReadModelError(filename)
     def __read_model__(self, file):
-        pass # read a Pattern
+        pass # read a compiled regex
     def parse(self, target, time_base=datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')):
         self.__target__ = target
         self.__time_base__ = time_base
@@ -31,3 +32,30 @@ class TimeNormalizer:
         self.__target__ = prehandler.rm(self.__target__, u'的+')
         self.__target__ = prehandler.number_translator(self.__target__)
     def __time_ex__(self, tar, timebase):
+        startline = endline = -1
+        temp = []
+        rpointer = 0
+
+        m = self.__patterns__.match(tar)
+        startmark = True
+        while m:
+            startline = m.start()
+            if endline == startline:
+                rpointer -= 1
+                temp[rpointer] = temp[rpointer] + m.group()
+            else:
+                if not startmark:
+                    rpointer -= 1
+                    # print temp[rpointer]
+                    rpointer += 1
+                startmark = False
+                temp[rpointer] = m.group()
+            endline = m.end()
+            rpointer += 1
+
+        if rpointer > 0:
+            rpointer -= 1
+            # print temp[rpointer]
+            rpointer += 1
+
+        Time_Result = [TimeUnit(t, self) for t in temp]
