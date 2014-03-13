05ac36870aa87d181d0945deb4827e94dccc06bf
leon-huang <leon@njuopen.com>
2014-03-13 18:37:25 +0800

    I think I finished

--- time_expr/unit.py
+++ time_expr/unit.py
@@ -17,7 +17,12 @@ class TimeUnit:
         self._tp = TimePoint()
         self._to_origin = TimePoint()
         self.__normalizer__ = n
+        self.time = None
+        self.time_full = None
+        self.time_origin = None
         Time_Normalization()
+    def __str__(self):
+        return self.Time_Expression + ' --> ' + self.Time_Norm
     def __norm_set__(self, regex, idx):
         pattern = re.compile(regex)
         m = pattern.match(self.Time_Expression)
@@ -198,14 +203,12 @@ class TimeUnit:
             year = int(match.group())
             calendar = datetime.datetime(calendar.year+year, calendar.month, calendar.day, calendar.hour, calendar.minute, calendar.second)
 
-        s = calendar.strftime('%Y-%m-%d-%H-%M-%S')
-        time_fin = s.split('-')
         if any(flag):
-            _tp.tunit[0] = int(time_fin[0])
+            _tp.tunit[0] = calendar.year
         if any(flag[:2])
-            _tp.tunit[1] = int(time_fin[1])
+            _tp.tunit[1] = calendar.month
         if flag[2]:
-            _tp.tunit[2] = int(time_fin[2])
+            _tp.tunit[2] = calendar.day
     def norm_setCurRelated(self):
         time_grid = self.__normalizer__.__old_time_base__.split('-')
         ini = [int(i) for i in time_grid]
@@ -310,173 +313,77 @@ class TimeUnit:
             flag[2] = True
             calendar -= datetime.timedelta(14+calendar.weekday()-int(match.group()))
 
-        ######## TODO
-        ######## TODO
-        rule="(?<=((?<!上)上(周|星期)))[1-7]";
-        pattern=Pattern.compile(rule);
-        match=pattern.matcher(Time_Expression);
-        if(match.find())
-        {
-            flag[2] = true;
-            int week = Integer.parseInt(match.group());
-            if(week == 7)
-                week = 1;
-            else
-                week++;
-            calendar.add(Calendar.WEEK_OF_MONTH, -1);
-            calendar.set(Calendar.DAY_OF_WEEK, week);
-        }
+        pattern = re.compile(u'(?<=((?<!上)上(周|星期)))[1-7]')
+        m = pattern.match(self.Time_Expression)
+        if m:
+            flag[2] = True
+            calendar -= datetime.timedelta(7+calendar.weekday()-int(match.group()))
 
-        rule="(?<=((?<!下)下(周|星期)))[1-7]";
-        pattern=Pattern.compile(rule);
-        match=pattern.matcher(Time_Expression);
-        if(match.find())
-        {
-            flag[2] = true;
-            int week = Integer.parseInt(match.group());
-            if(week == 7)
-                week = 1;
-            else
-                week++;
-            calendar.add(Calendar.WEEK_OF_MONTH, 1);
-            calendar.set(Calendar.DAY_OF_WEEK, week);
-        }
+        pattern = re.compile(u'(?<=((?<!下)下(周|星期)))[1-7]')
+        m = pattern.match(self.Time_Expression)
+        if m:
+            flag[2] = True
+            calendar += datetime.timedelta(7-calendar.weekday()+int(match.group()))
 
-        rule="(?<=(下下(周|星期)))[1-7]";
-        pattern=Pattern.compile(rule);
-        match=pattern.matcher(Time_Expression);
-        if(match.find())
-        {
-            flag[2] = true;
-            int week = Integer.parseInt(match.group());
-            if(week == 7)
-                week = 1;
-            else
-                week++;
-            calendar.add(Calendar.WEEK_OF_MONTH, 2);
-            calendar.set(Calendar.DAY_OF_WEEK, week);
-        }
+        pattern = re.compile(u'(?<=(下下(周|星期)))[1-7]')
+        m = pattern.match(self.Time_Expression)
+        if m:
+            flag[2] = True
+            calendar += datetime.timedelta(7-calendar.weekday()+int(match.group()))
 
-        rule="(?<=((?<!(上|下))(周|星期)))[1-7]";
-        pattern=Pattern.compile(rule);
-        match=pattern.matcher(Time_Expression);
-        if(match.find())
-        {
-            flag[2] = true;
-            int week = Integer.parseInt(match.group());
-            if(week == 7)
-                week = 1;
-            else
-                week++;
-            calendar.add(Calendar.WEEK_OF_MONTH, 0);
-            calendar.set(Calendar.DAY_OF_WEEK, week);
-        }
+        pattern = re.compile(u'(?<=((?<!(上|下))(周|星期)))[1-7]')
+        m = pattern.match(self.Time_Expression)
+        if m:
+            flag[2] = True
+            calendar += datetime.timedelta(-calendar.weekday()+int(match.group()))
 
-        String s = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(calendar.getTime());
-        String[] time_fin = s.split("-");
-        if(flag[0]||flag[1]||flag[2]){
-            _tp.tunit[0] = Integer.parseInt(time_fin[0]);
-        }
-        if(flag[1]||flag[2])
-            _tp.tunit[1] = Integer.parseInt(time_fin[1]);
-        if(flag[2])
-            _tp.tunit[2] = Integer.parseInt(time_fin[2]);
+        if any(flag):
+            _tp.tunit[0] = calendar.year
+        if any(flag[1:])
+            _tp.tunit[1] = calendar.month
+        if flag[2]:
+            _tp.tunit[2] = calendar.day
+    def modifyTimeBase(self):
+        time_grid = self.__normalizer__.__time_base__.split('-')
+        s = '-'.join([ str(_tp.tunit[i]) if _tp.tunit[i] != -1 else t for i, t in enumerate(time_grid)])
+        self.__normalizer__.__time_base__ = s
+    def Time_Normalization(self):
+        self.norm_setyear()
+        self.norm_setmonth()
+        self.norm_setday()
+        self.norm_sethour()
+        self.norm_setminute()
+        self.norm_setsecond()
+        self.norm_setTotal()
+        self.norm_setBaseRelated()
+        self.norm_setCurRelated()
+        self.modifyTimeBase()
 
-    }
-    public void modifyTimeBase(){
-        String [] time_grid=new String[6];
-        time_grid=normalizer.getTimeBase().split("-");
+        _tp_origin.tunit = _tp.tunit[:]
 
-        String s = "";
-        if(_tp.tunit[0] != -1)
-            s += Integer.toString(_tp.tunit[0]);
-        else
-            s += time_grid[0];
-        for(int i = 1; i < 6; i++){
-            s += "-";
-            if(_tp.tunit[i] != -1)
-                s += Integer.toString(_tp.tunit[i]);
-            else
-                s += time_grid[i];
-        }
-        normalizer.setTimeBase(s);
-    }
+        time_grid = self.__normalizer__.__time_base__.split('-')
 
-    public void Time_Normalization()
-    {
-        norm_setyear();
-        norm_setmonth();
-        norm_setday();
-        norm_sethour();
-        norm_setminute();
-        norm_setsecond();
-        norm_setTotal();
-        norm_setBaseRelated();
-        norm_setCurRelated();
-        modifyTimeBase();
+        tunitpointer = 5
+        while tunitpointer >= 0 and _tp.tunit[tunitpointer] < 0:
+            tunitpointer -= 1
+        for i in range(tunitpointer):
+            if _tp.tunit[i] < 0:
+                _tp.tunit[i] = int(time_grid[i])
 
-        _tp_origin.tunit = _tp.tunit.clone();
+        _result_tmp = _tp.tunit[:]
 
-        String [] time_grid=new String[6];
-        time_grid=normalizer.getTimeBase().split("-");
+        threshold = datetime.datetime.now().year % 100
+        if _tp.tunit[0] > threshold and _tp.tunit[0] < 100:
+            _result_tmp[0] = 1900 + _tp.tunit[0]
+        elif _tp.tunit[0] > 0 and _tp.tunit[0] <= threshold:
+            _result_tmp[0] = 2000 + _tp.tunit[0]
 
-        int tunitpointer=5;
-        while (tunitpointer>=0 && _tp.tunit[tunitpointer]<0)
-        {
-            tunitpointer--;
-        }
-        for (int i=0;i<tunitpointer;i++)
-        {
-            if (_tp.tunit[i]<0)
-                _tp.tunit[i]=Integer.parseInt(time_grid[i]);
-        }
-        String[] _result_tmp=new String[6];
-        _result_tmp[0]=String.valueOf(_tp.tunit[0]);
-        if (_tp.tunit[0]>=10 &&_tp.tunit[0]<100)
-        {
-            _result_tmp[0]="19"+String.valueOf(_tp.tunit[0]);
-        }
-        if (_tp.tunit[0]>0 &&_tp.tunit[0]<10)
-        {
-            _result_tmp[0]="200"+String.valueOf(_tp.tunit[0]);
-        }
-
-        for (int i = 1; i < 6; i++) {
-            _result_tmp[i] = String.valueOf(_tp.tunit[i]);
-        }
-
-        Calendar cale = Calendar.getInstance();         //leverage a calendar object to figure out the final time
-        cale.clear();
-        if (Integer.parseInt(_result_tmp[0]) != -1) {
-            Time_Norm += _result_tmp[0] + "年";
-            cale.set(Calendar.YEAR, Integer.valueOf(_result_tmp[0]));
-            if (Integer.parseInt(_result_tmp[1]) != -1) {
-                Time_Norm += _result_tmp[1] + "月";
-                cale.set(Calendar.MONTH, Integer.valueOf(_result_tmp[1]) - 1);
-                if (Integer.parseInt(_result_tmp[2]) != -1) {
-                    Time_Norm += _result_tmp[2] + "日";
-                    cale.set(Calendar.DAY_OF_MONTH, Integer.valueOf(_result_tmp[2]));
-                    if (Integer.parseInt(_result_tmp[3]) != -1) {
-                        Time_Norm += _result_tmp[3] + "时";
-                        cale.set(Calendar.HOUR_OF_DAY, Integer.valueOf(_result_tmp[3]));
-                        if (Integer.parseInt(_result_tmp[4]) != -1) {
-                            Time_Norm += _result_tmp[4] + "分";
-                            cale.set(Calendar.MINUTE, Integer.valueOf(_result_tmp[4]));
-                            if (Integer.parseInt(_result_tmp[5]) != -1) {
-                                Time_Norm += _result_tmp[5] + "秒";
-                                cale.set(Calendar.SECOND, Integer.valueOf(_result_tmp[5]));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        time = cale.getTime();
-
-        time_full = _tp.tunit.clone();
-        time_origin = _tp_origin.tunit.clone();
-    }
-
-    public String toString(){
-        return Time_Expression+" ---> "+ Time_Norm;
-    }
+        first_n1_idx = _result_tmp.index(-1)
+        if first_n1_idx < 3:
+            _result_tmp = [x if x != -1 else 1 for x in _result_tmp]
+            first_n1_idx = 3
+        cale = datetime.datetime(*_result_tmp[:first_n1_idx])
+        self.Time_Norm = cale.strftime(u'%Y年%m月%d日%H时%M分%S秒')
+        self.time = cale
+        self.time_full = _tp.tunit[:]
+        self.time_origin = _tp_origin[:]
